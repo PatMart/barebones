8b071187b299cb2006c591ad5faffb107918020d
Anna Debenham <anna@maban.co.uk>
2013-09-02 14:14:56 +0100

    Updating to the latest version of Paul's barebones

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 <!DOCTYPE html><html lang="en-gb">
 
 <head>
-    <title>Barebones - An initial directory setup, style guide and pattern primer</title>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="_css/reset.css" type="text/css"/>
-    <link rel="stylesheet" href="_css/patterns.css" type="text/css"/>
+	<title>Barebones - An initial directory setup, style guide and pattern primer</title>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<link rel="stylesheet" href="_css/styles.css" type="text/css"/>
 </head>
 
 <body>
-    <header>
-        <h1>Barebones</h1>
-    </header>
+	<header role="banner">
+		<div class="container">
+			<h1 class="headline">Barebones</h1>
+			<p class="lede">An initial directory setup, style guide and pattern&#160;primer</p>
+		</div><!--/container-->
+	</header><!--/@banner-->
 
-    <p>An initial directory setup, style guide and pattern primer. Intended as a starting point for my own projects, available for others to fork and adapt for theirs.</p>
-    <p>Use Barebones to start your own project! <a href="https://github.com/paulrobertlloyd/barebones/archive/master.zip" download="barebones">Download the files</a> or <a href="https://github.com/paulrobertlloyd/barebones">view the project</a> on Github</a>.<p>
-    <h2>Contents</h2>
-    <dl>
-        <dt><a href="_styleguide.php">Style Guide</a></dt>
-        <dd>Base markup styles used throughout this site.</dd>
-        <dt><a href="_patterns.php">Pattern Library</a></dt>
-        <dd>Common snippets of markup used throughout this site.</dd>
-        <dt><a href="_forms.php">Form Example</a></dt>
-        <dd>Forms are used when you wish to collect data from users.</dd>
-    </dl>
+	<main role="main">
+		<div class="container">
+			<p>Intended as a starting point for my own projects, Barebones is freely available to fork and adapt for your own&#160;needs.</p>
+			<p><a class="button" href="https://github.com/paulrobertlloyd/barebones/archive/master.zip" download="barebones">Download Barebones</a> or&#160;<a href="https://github.com/paulrobertlloyd/barebones">view&#160;project&#160;on&#160;Github</a>.<p>
+			<h2>Contents</h2>
+			<dl>
+				<dt><a href="_styleguide.php">Style Guide</a></dt>
+				<dd>Basic markup and typographic styles</dd>
+				<dt><a href="_patterns.php">Pattern Library</a></dt>
+				<dd>Common snippets of markup</dd>
+				<dt><a href="_forms.php">Form Example</a></dt>
+				<dd>Basic form styles</dd>
+			</dl>
+		</div><!--/container-->
+	</main><!--@main-->
 
-    <footer role="contentinfo">
-        <p><small>Copyright &#169; 2012 Paul Robert Lloyd. Code covered by the <a rel="license" href="http://paulrobertlloyd.mit-license.org/">MIT license</a>.</small></p>
-    </footer><!--/.contentinfo-->
+	<footer role="contentinfo">
+		<div class="container">
+			<p><small>Copyright &#169; 2013 <a href="http://paulrobertlloyd.com">Paul Robert Lloyd</a>. Code covered by the <a rel="license" href="http://paulrobertlloyd.mit-license.org/">MIT license</a>.</small></p>
+		</div><!--/container-->
+	</footer><!--/@contentinfo-->
 </body>
 </html>
